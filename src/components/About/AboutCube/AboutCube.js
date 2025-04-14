f44ea8626b9570273ca4c2b1d02b4030b72ce34e
Jaskaran Singh <jsingh@jsingh-ML.local>
2025-04-14 14:06:26 -0400

    fix: about cube images

--- src/components/About/AboutCube/AboutCube.js
+++ src/components/About/AboutCube/AboutCube.js
@@ -7,22 +7,22 @@ const AboutCube = () => {
         <div className={classes.AboutCube}>
             <div className={[classes.Box, classes.ShowFront].join(' ')}>
                 <div className={[classes.Face, classes.Front].join(' ')}>
-                    <Image width={180} height={180} src="/assets/images/html.png" alt="Html" />
+                    <Image width={180} height={180} src={`${process.env.BASE_PATH}/assets/images/html.png`} alt="Html" />
                 </div>
                 <div className={[classes.Face, classes.Back].join(' ')}>
-                    <Image width={180} height={180} src="/assets/images/css.png" alt="css" />
+                    <Image width={180} height={180} src={`${process.env.BASE_PATH}/assets/images/css.png`} alt="css" />
                 </div>
                 <div className={[classes.Face, classes.Right].join(' ')}>
-                    <Image width={180} height={180} src="/assets/images/js.png" alt="JavaScript" />
+                    <Image width={180} height={180} src={`${process.env.BASE_PATH}/assets/images/js.png`} alt="JavaScript" />
                 </div>
                 <div className={[classes.Face, classes.Left].join(' ')}>
-                    <Image width={180} height={180} src="/assets/images/node.png" alt="Node" />
+                    <Image width={180} height={180} src={`${process.env.BASE_PATH}/assets/images/node.png`} alt="Node" />
                 </div>
                 <div className={[classes.Face, classes.Top].join(' ')}>
-                    <Image width={180} height={180} src="/assets/images/webpack.png" alt="Webpack" />
+                    <Image width={180} height={180} src={`${process.env.BASE_PATH}/assets/images/webpack.png`} alt="Webpack" />
                 </div>
                 <div className={[classes.Face, classes.Bottom].join(' ')}>
-                    <Image width={180} height={180} src="/assets/images/react.png" alt="React" />
+                    <Image width={180} height={180} src={`${process.env.BASE_PATH}/assets/images/react.png`} alt="React" />
                 </div>
             </div>
         </div>
